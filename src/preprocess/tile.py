6cfd7469deb8491c46d57a0bd995870cef59304f
tomwilsonsco <17555474+tomwilsonsco@users.noreply.github.com>
2024-11-11 16:22:32 +0000

    formatting change

--- src/preprocess/tile.py
+++ src/preprocess/tile.py
@@ -13,10 +13,12 @@ def main():
         pixel_dimensions=128,
         offset=64,
         output_format="tif",
-        max_batch_size=1000
+        max_batch_size=1000,
     )
 
-    image_chipper.set_normaliser(min_val=[250,250,250,0], max_val=[2500, 2500, 2500, 7000])
+    image_chipper.set_normaliser(
+        min_val=[250, 250, 250, 0], max_val=[2500, 2500, 2500, 7000]
+    )
 
     # Generate chips
     image_chipper.chip_image()
@@ -37,7 +39,7 @@ def main():
 
     # Remove chips with only background
     remover.remove_background_only_files(
-        class_chips_dir="outputs/chips_seg", image_chips_dir="outputs/chips_img"
+        class_chips_dir="inputs/chips_seg", image_chips_dir="inputs/chips_img"
     )
 
 
