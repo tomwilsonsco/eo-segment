6ef2bbe5cc73ac64c5fb12e82c18c0e2d80b3aad
tomwilsonsco <17555474+tomwilsonsco@users.noreply.github.com>
2024-11-12 09:16:25 +0000

    format doc

--- src/fast_ai/predict.py
+++ src/fast_ai/predict.py
@@ -13,7 +13,15 @@ from train import (
 )  # need this part of the data load for prediction
 
 
-def write_prediction(image_path, output_path, pred_arr):
+def _write_prediction(image_path, output_path, pred_arr):
+    """
+    Write the prediction array to an output image file.
+
+    Args:
+        image_path (pathlib.Path): Path to the input image.
+        output_path (pathlib.Path): Path to save the predicted image.
+        pred_arr (numpy.ndarray): Prediction array to write to the output file.
+    """
     with rio.open(image_path) as f:
         prof = f.profile
     prof.update(count=1, dtype=np.int8)
@@ -22,20 +30,52 @@ def write_prediction(image_path, output_path, pred_arr):
 
 
 def _generate_windows(src, tile_size, offset):
+    """
+    Generate sliding windows over the source image.
+
+    Args:
+        src (rasterio.io.DatasetReader): Source image opened with Rasterio.
+        tile_size (int): Size of the window tiles.
+        offset (int): Offset for sliding windows.
+
+    Yields:
+        rasterio.windows.Window: A window representing a portion of the image.
+    """
     for y in range(0, src.height, offset):
         for x in range(0, src.width, offset):
             window = Window(x, y, tile_size, tile_size)
             yield window
 
 
-def apply_normaliser_scaler(img_arr, normaliser_scaler):
+def _apply_normaliser_scaler(img_arr, normaliser_scaler):
+    """
+    Apply a normaliser or scaler to the input image array. Uses rschip package.
+
+    Args:
+        img_arr (numpy.ndarray): Image array to normalize or scale.
+        normaliser_scaler (dict): Dictionary containing either a normaliser or scaler.
+
+    Returns:
+        numpy.ndarray: Normalised or scaled image array.
+    """
     if "normaliser" in normaliser_scaler:
         return ImageChip.apply_normaliser(img_arr, normaliser_scaler["normaliser"])
     else:
         return ImageChip.apply_scaler(img_arr, normaliser_scaler["scaler"])
 
 
-def centre_only_prediction(pred_arr, win, src):
+def _centre_only_prediction(pred_arr, win, src):
+    """
+    Retain only the central region of the prediction, removing boundary effects.
+
+    Args:
+        pred_arr (numpy.ndarray): Prediction array from the model.
+        win (rasterio.windows.Window): Window representing the current portion of the image.
+        src (rasterio.io.DatasetReader): Source image opened with Rasterio.
+
+    Returns:
+        numpy.ndarray: Prediction array with only the central region retained.
+    """
     tile_size = pred_arr.shape[0]
     mask = np.zeros((tile_size, tile_size), dtype=np.int8)
     # Default central region
@@ -59,25 +99,57 @@ def centre_only_prediction(pred_arr, win, src):
     return pred_arr * mask
 
 
-def read_predict_window(image_path, window, model, normaliser_scaler):
+def _read_predict_window(image_path, window, model, normaliser_scaler):
+    """
+    Read a window of the image and make a prediction using the model.
+
+    Args:
+        image_path (pathlib.Path): Path to the input image.
+        window (rasterio.windows.Window): Window representing the portion of the image to read.
+        model (fastai.learner.Learner): Trained fastai model for prediction.
+        normaliser_scaler (dict): Dictionary containing either a normaliser or scaler.
+
+    Returns:
+        numpy.ndarray: Prediction array for the given window.
+    """
     with rio.open(image_path) as f:
         img_arr = f.read(window=window, boundless=True, fill_value=0)
     if normaliser_scaler:
-        img_arr = apply_normaliser_scaler(img_arr, normaliser_scaler)
+        img_arr = _apply_normaliser_scaler(img_arr, normaliser_scaler)
     pred = model.predict(img_arr)[
         0
     ].numpy()  # predict method returns (label, _, probabilities)
     return np.squeeze(pred)
 
 
-def win_ranges(win):
+def _win_ranges(win):
+    """
+    Get the range of rows and columns for a given window.
+
+    Args:
+        win (rasterio.windows.Window): Window representing a portion of the image.
+
+    Returns:
+        tuple: ymin, ymax, xmin, xmax representing the row and column ranges of the window.
+    """
     ymin, ymax = (win.row_off, win.row_off + win.height)
     xmin, xmax = (win.col_off, win.col_off + win.width)
     return ymin, ymax, xmin, xmax
 
 
-def init_pred_array(pred_full, win, tile_size):
-    ymin, ymax, xmin, xmax = win_ranges(win)
+def _init_pred_array(pred_full, win, tile_size):
+    """
+    Initialize a prediction array for a given window.
+
+    Args:
+        pred_full (numpy.ndarray): Full prediction array for the entire image.
+        win (rasterio.windows.Window): Window representing the portion of the image to initialize.
+        tile_size (int): Size of the tile.
+
+    Returns:
+        tuple: Initialized array, height, and width of the values extracted from the full prediction.
+    """
+    ymin, ymax, xmin, xmax = _win_ranges(win)
     init_arr = np.zeros((tile_size, tile_size))
     value_ext = pred_full[0, ymin:ymax, xmin:xmax]
     value_ht, value_wd = value_ext.shape
@@ -85,26 +157,50 @@ def init_pred_array(pred_full, win, tile_size):
     return init_arr, value_ht, value_wd
 
 
-def predict_image(image_path, output_path, model, normaliser_scaler, tile_size, offset):
+def predict_image(
+    image_path,
+    output_path,
+    model,
+    normaliser_scaler,
+    tile_size,
+    offset,
+    boundary_remove,
+):
+    """
+    Predict the entire image by iterating over sliding windows.
+
+    Args:
+        image_path (pathlib.Path): Path to the input image.
+        output_path (pathlib.Path): Path to save the predicted output image.
+        model (fastai.learner.Learner): Trained fastai model for prediction.
+        normaliser_scaler (dict): Dictionary containing either a normaliser or scaler.
+        tile_size (int): Size of the tiles to split the image into.
+        offset (int): Offset for sliding windows.
+        boundary_remove (bool): Whether to remove the outer 1/8th of pixels in the prediction tiles.
+    """
     with rio.open(image_path) as src:
         windows = list(_generate_windows(src, tile_size, offset))
         pred_full = np.zeros((1, src.height, src.width))
         for i, win in enumerate(windows):
-            init, val_ht, val_wd = init_pred_array(pred_full, win, tile_size)
-            pred = read_predict_window(image_path, win, model, normaliser_scaler)
+            init, val_ht, val_wd = _init_pred_array(pred_full, win, tile_size)
+            pred = _read_predict_window(image_path, win, model, normaliser_scaler)
             pred = pred.astype(np.uint8)
-            pred = centre_only_prediction(pred, win, src)
+            if boundary_remove:
+                pred = _centre_only_prediction(pred, win, src)
             pred = np.maximum(pred, init)
             pred = pred[0:val_ht, 0:val_wd]
-            ymin, ymax, xmin, xmax = win_ranges(win)
+            ymin, ymax, xmin, xmax = _win_ranges(win)
             pred_full[0, ymin:ymax, xmin:xmax] = pred
             if i != 0 and i % 100 == 0:
                 print(f"predicted {i} of {len(windows)}")
-    write_prediction(image_path, output_path, pred_full)
+    _write_prediction(image_path, output_path, pred_full)
     print(f"Created {output_path}")
 
 
 def main():
+    """
+    Main function to parse command-line arguments and perform image prediction using a trained model.
+    """
     # Parse command-line arguments
     parser = argparse.ArgumentParser(
         description="Predict a full image using trained fastai unet model"
@@ -139,6 +235,13 @@ def main():
         type=int,
         default=64,
         help="Offset for sliding windows.",
+    )
+
+    parser.add_argument(
+        "--boundary-remove",
+        action="store_true",
+        help="Remove the outer 1/8th of pixels in the prediction tiles to clean edges\
+         and retain pred from overlapping tile",
     )
 
     args = parser.parse_args()
@@ -176,13 +279,19 @@ def main():
     if output_path.exists():
         try:
             Path.unlink(output_path)
-        except:
+        except FileExistsError:
             raise FileExistsError("Prediction image already exists. Cannot overwrite.")
 
     model = load_learner(model_path, cpu=False)
 
     predict_image(
-        image_path, output_path, model, normaliser_scaler, args.tile_size, args.offset
+        image_path,
+        output_path,
+        model,
+        normaliser_scaler,
+        args.tile_size,
+        args.offset,
+        args.boundary_remove,
     )
 
 
